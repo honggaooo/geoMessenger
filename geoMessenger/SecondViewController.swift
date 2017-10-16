d450b31b2cd5969d0dc232be75b6f64dba3242b8
honggaooo <hong.gao@marquette.edu>
2017-10-16 02:08:08 -0500

    Initial Commit

---
+++ geoMessenger/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  geoMessenger
+//
+//  Created by Hong Gao on 10/16/17.
+//  Copyright © 2017 Hong Gao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
